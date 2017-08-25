0066ba380a3df929751610e5f9be0030074f4a81
ilich2 <31213344+ilich2@users.noreply.github.com>
2017-08-25 15:52:41 +0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,9 +1,13 @@
 CREATE TABLE `users` (
-	`id` int(11) NOT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`email` varchar(255) DEFAULT NULL,
 	`password` varchar(255) DEFAULT NULL,
 	`level` enum('admin', 'user') NOT NULL,
 	`access` tinyint(4) NOT NULL,
 	`registrationDate` int(11) NOT NULL,
-	`code` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	`code` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`id`, `email`, `password`, `level`, `access`, `registrationDate`, `code`) VALUES
+(1, 'admin@test.ru', 'b59c67bf196a4758191e42f76670ceba', 'admin', 1, 1503304322, '');
